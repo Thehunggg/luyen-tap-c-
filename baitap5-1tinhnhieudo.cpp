aa411fff0ce19a7d16e1748c483d96ed9ea0bf84
ThehungIT11A <hdothe1@gmail.com>
2026-06-25 10:05:19 +0900

    add them bai 5 vao git

---
+++ baitap5-1tinhnhieudo.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    double tempc,tempf;
+    cout<<"Enter tempratue f:";
+
+    cin>>tempf;
+    tempc=(tempf-32)/1.8;
+    cout<<"Tempature C is:"<<tempc;
+    return 0;
+     
+}
